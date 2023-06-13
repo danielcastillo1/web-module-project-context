c11942b206f4b2bb7a8202c7b1702f4ee9f8799a
Daniel Castillo <danielcastillo89@icloud.com>
2023-06-12 22:56:19 -0500

    wip

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductContext from '../contexts/productContext';
 
-const Product = props => {
+const Product = () => {
+	const { addItem } = useContext(ProductContext);
+
 	return (
 		<div className="product">
-			<img src={props.product.image} alt={`${props.product.title} book`} />
+			<img src={ProductContext.product.image} alt={`${ProductContext.product.title} book`} />
 
-			<h1 className="title">{props.product.title}</h1>
+			<h1 className="title">{ProductContext.product.title}</h1>
 
-			<p className="price">${props.product.price}</p>
+			<p className="price">${ProductContext.product.price}</p>
 
-			<button onClick={() => props.addItem(props.product)}>
+			<button onClick={() => addItem(ProductContext.product)}>
 				Add to cart
 			</button>
 		</div>
